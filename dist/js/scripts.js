882623f7d4c72da74e85ab796d08e64c9ca45651
th3qui85ly <shivansh.srivastava1912@gmail.com>
2022-04-03 03:35:51 +0530

    Update .pug file

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+* Start Bootstrap - Portfolio vundefined (undefined)
+* Copyright 2013-2022 undefined
+* Licensed under undefined (https://github.com/StartBootstrap/shivansh-portfolio/blob/master/LICENSE)
 */
 //
 // Scripts
